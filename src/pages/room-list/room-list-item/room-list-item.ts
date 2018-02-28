977db8641327049a47c1cce2093b07b26780574d
Adam Grieger <adamgrieger@gmail.com>
2018-02-27 17:01:35 -0800

    Enable join room button only when logged in

--- src/pages/room-list/room-list-item/room-list-item.ts
+++ src/pages/room-list/room-list-item/room-list-item.ts
@@ -1,17 +1,26 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
 
 import { RoomListing } from '../../../core/api/listjockey/models/room-list.models';
+import { AppState } from '../../../core/redux/store/models';
 
 @Component({
   selector: 'room-list-item',
   templateUrl: 'room-list-item.html'
 })
-export class RoomListItem {
+export class RoomListItem implements OnInit {
 
   @Input() listing: RoomListing;
   @Output() onJoin = new EventEmitter<number>();
 
-  constructor() { }
+  private accessToken$: Observable<string>;
+
+  constructor(private ngRedux: NgRedux<AppState>) { }
+
+  ngOnInit() {
+    this.accessToken$ = this.ngRedux.select(state => state.session.accessToken);
+  }
 
   private join = (id: number) => this.onJoin.emit(id);
 }
