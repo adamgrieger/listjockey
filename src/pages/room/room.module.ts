26f6ae3ad899c965c5d1eddf9b77ab268ddd6738
Adam Grieger <adamgrieger@gmail.com>
2018-04-22 17:47:56 -0700

    Add song-queue component

--- src/pages/room/room.module.ts
+++ src/pages/room/room.module.ts
@@ -3,13 +3,15 @@ import { IonicPageModule } from 'ionic-angular';
 
 import { DeviceSelect } from './device-select/device-select';
 import { RoomPage } from './room';
+import { SongQueue } from './song-queue/song-queue';
 import { SongSearch } from './song-search/song-search';
 
 @NgModule({
   declarations: [
     RoomPage,
     SongSearch,
-    DeviceSelect
+    DeviceSelect,
+    SongQueue
   ],
   imports: [
     IonicPageModule.forChild(RoomPage)
---
+++ src/pages/room/song-queue/song-queue.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
+
+import { Room } from '../../../core/api/listjockey/models/rooms.models';
+import { RoomActions } from '../../../core/redux/room/services/actions.service';
+import { AppState } from '../../../core/redux/store/models';
+
+@Component({
+  selector: 'song-queue',
+  templateUrl: 'song-queue.html'
+})
+export class SongQueue implements OnInit {
+
+  private room$: Observable<Room>;
+
+  constructor(
+    private ngRedux: NgRedux<AppState>,
+    private room: RoomActions
+  ) { }
+
+  ngOnInit() {
+    this.room$ = this.ngRedux.select(state => state.room.current);
+  }
+}
