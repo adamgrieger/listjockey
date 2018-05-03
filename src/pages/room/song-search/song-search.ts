d1b9f709637c9290ae9a57f76e78ba357d78a748
Adam Grieger <adamgrieger@gmail.com>
2018-05-02 20:31:21 -0700

    Make song-search more responsive and user friendly

--- src/pages/room/song-search/song-search.ts
+++ src/pages/room/song-search/song-search.ts
@@ -1,16 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgRedux } from 'ng2-redux';
 import { Observable } from 'rxjs/Observable';
 
 import { RoomActions } from '../../../core/redux/room/services/actions.service';
 import { SearchActions } from '../../../core/redux/search/services/actions.service';
 import { AppState } from '../../../core/redux/store/models';
+import { Searchbar } from 'ionic-angular';
 
 @Component({
   selector: 'song-search',
   templateUrl: 'song-search.html'
 })
 export class SongSearch implements OnInit, OnDestroy {
+
+  @ViewChild(Searchbar) searchbar: Searchbar;
 
   private tracks$: Observable<SpotifyApi.TrackObjectFull[]>;
 
@@ -31,6 +34,8 @@ export class SongSearch implements OnInit, OnDestroy {
   private searchTracks = (query: string) => {
     if (query) {
       this.search.searchTracks(query);
+    } else {
+      this.clearSearch();
     }
   }
 
@@ -38,7 +43,9 @@ export class SongSearch implements OnInit, OnDestroy {
     this.search.clearSearch();
   }
 
-  private addSong = (track: SpotifyApi.TrackObjectFull) =>
+  private addSong = (track: SpotifyApi.TrackObjectFull) => {
+    this.searchbar.value = '';
+    this.clearSearch();
     this.room.addSong({
       track_id: track.id,
       added_by: this.ngRedux.getState().session.user.id,
@@ -50,5 +57,6 @@ export class SongSearch implements OnInit, OnDestroy {
       },
       duration: Math.floor(track.duration_ms / 1000),
       offset: 0
-    })
+    });
+  }
 }
